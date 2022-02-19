6697432c74214dd2f2606ef67888389c7bc21ec3
Sanuja Methmal <methmal66@yahoo.com>
2022-02-19 08:02:19 +0530

    exercises in lecture1

---
+++ lectures/1/1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    cout << "My name is Methmal" << endl;
+    cout << "Um from Ja-ela" << endl;
+    return 0;
+}
